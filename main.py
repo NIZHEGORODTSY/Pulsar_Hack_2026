70516cd1b37e753380266bfbc5520bbd000701b8
LilFreezyV <an.volodya.anufriev@yandex.ru>
2026-02-07 12:48:44 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('aaa')
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Мое Android приложение"
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    # Элементы интерфейса
+    txt_name = ft.TextField(label="Ваше имя")
+    txt_greeting = ft.Text()
+
+    def say_hello(e):
+        txt_greeting.value = f"Привет, {txt_name.value}!"
+        page.update()
+
+    page.add(
+        ft.Column([
+            ft.Text("Добро пожаловать!", size=30),
+            txt_name,
+            ft.ElevatedButton("Поздороваться", on_click=say_hello),
+            txt_greeting
+        ])
+    )
+
+
+
+
